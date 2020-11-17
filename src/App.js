0ce9893f1e86cc5cee6c4e8841f70a317cd60910
surya0249 <sajja.surya2011@gmail.com>
2020-11-17 04:37:40 +0000

    added

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          this is surya sajja fan of msd!!@@@@
+          this is surya sajja fan of mahi!!@@@@
         </p>
         <a
           className="App-link"
